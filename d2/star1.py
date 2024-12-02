9697d4acd27ab621056a0115e7da41114196c51f
Quentin V <50825643+qquentinv@users.noreply.github.com>
2024-12-02 12:56:39 +0000

    feat(d2) add last condition in star 1

--- d2/star1.py
+++ d2/star1.py
@@ -1,7 +1,7 @@
 input = [
     [7,6,4,2,1], # SAFE
-    [1,2,7,8,9], # SAFE
-    [9,7,6,2,1], # SAFE
+    [1,2,7,8,9], # UNSAFE
+    [9,7,6,2,1], # UNSAFE
     [1,3,2,4,5], # UNSAFE
     [8,6,4,4,1], # UNSAFE
     [1,3,6,7,9], # SAFE
@@ -13,10 +13,10 @@ safe = 0
 
 # On parcourt toutes les lignes
 for line in input:
-    print("INPUT ENTRY") 
+    #print("INPUT ENTRY") 
     for i, elt in enumerate(line): 
         ## CONDITION 1 : Croissant ou Décroissant
-        print(f"start {i} with {elt}")
+        #print(f"start {i} with {elt}")
         if(error is True):
             continue
         if(i == 0): 
@@ -27,25 +27,33 @@ for line in input:
                 trend = 'decreasing'
             else:
                 trend = 'increase'
-        print(f" prevElt {prevElt} // elt {elt} // trend {trend}")
+        #print(f" prevElt {prevElt} // elt {elt} // trend {trend}")
         if(trend == 'decreasing'):
             if(prevElt <= elt):
                 error = True
-                print("ERROR")
+                #print("ERROR")
         else:
             if(prevElt >= elt):
                 error = True
-                print("ERROR")
+                #print("ERROR")
 
+        ## CONDITION 2: diff entre deux niveaux
+        diff = abs(prevElt - elt) 
+        #print(diff)
+        if(1 <= diff <= 3):
+            test = None
+            #print("OK")
+        else:
+            error = True
+
+        ## COMPTEUR SAFE
         prevElt = elt
         if(i == len(line) - 1 and error is False):
             safe += 1
-            print("SAFE")
-        
-        ## CONDITION 2: diff entre deux niveaux
+            #print("SAFE")
         
 
-    print(f"Trend: {trend}")
+    #print(f"Trend: {trend}")
     trend = None
     error = False
 
