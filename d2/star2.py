53011ecdffaf720e7a4f36a5d3f6a74873c8d558
Quentin V <50825643+qquentinv@users.noreply.github.com>
2024-12-04 16:24:11 +0000

    feat(d2): success star 2

--- d2/star2.py
+++ d2/star2.py
@@ -1,17 +1,25 @@
 input = [
-[59, 55, 52, 49, 47, 44]
+[7, 6, 4, 2, 1],
+[1, 2, 7, 8, 9],
+[9, 7, 6, 2, 1],
+[1, 3, 2, 4, 5],
+[8, 6, 4, 4, 1],
+[1, 3, 6, 7, 9],
 ]
+
+input = []
+
+with open("input.txt", "r") as file:
+    for line in file:
+        array_lines = list(map(int, line.split()))
+        input.append(array_lines)
 
 safe = 0
 
-def testMethod(line, last):
+def testMethod(line):
     trend = None
     error = False
 
-    if(last is False):
-        print(f"---list {line}")
-    else:
-        print(f"---new list {line}")
     for i, elt in enumerate(line): 
         ## CONDITION 1 : Croissant ou Décroissant
         print(f"start {i} with {elt}")
@@ -27,31 +35,11 @@ def testMethod(line, last):
         if(trend == 'decreasing'):
             if(prevElt <= elt):
                 print("ERROR DECREASING")
-                if(last is False):
-                    if(i == 2):
-                        if(elt <= line[i+1]):
-                            line.pop(i - 1)
-                        else:
-                            line.pop(i)
-                    else:
-                        line.pop(i)
-                    return testMethod(line, True)
-                else:
-                    return False;
+                return False;
         else:
             if(prevElt >= elt):
                 print("ERROR INCREASE")
-                if(last is False):
-                    if(i == 2):
-                        if(elt >= line[i+1]):
-                            line.pop(i - 2)
-                        else:
-                            line.pop(i)
-                    else:
-                        line.pop(i)
-                    return testMethod(line, True)
-                else:
-                    return False;
+                return False;
 
         ## CONDITION 2: diff entre deux niveaux
         diff = abs(prevElt - elt) 
@@ -61,18 +49,7 @@ def testMethod(line, last):
             print("OK")
         else:
             print("ERROR DIFF NIV")
-            if(last is False):
-                if(diff < 1):
-                    line.pop(i-1)
-                elif(diff > 3 and i == 1 and 0 > (elt - line[i+1]) <=3):
-                    line.pop(i-2)
-                elif(diff > 3 and i != len(line)-1 and 0 > (elt - line[i+1]) <=3 and 0 > (prevElt - line[i+1]) <= 3):
-                    line.pop(i-1)
-                else:
-                    line.pop(i)
-                return testMethod(line, True)
-            else:
-                return False;
+            return False;
 
         ## COMPTEUR SAFE
         prevElt = elt
@@ -80,26 +57,23 @@ def testMethod(line, last):
             print("-----------SAFE")
             return True
 
-
-# TODO FAIRE fonctionner cette méthode et garder une logique simple dans la testMethod
-def searchError(newLine):
-    return newLine
+def generateNewLines(line):
+    return [line[:i] + line[i+1:] for i in range(len(line))]
 
 # On parcourt toutes les lignes
 for line in input:
     print("-----------INPUT ENTRY") 
-    result = testMethod(line, False)
+    result = testMethod(line)
     if(result is True):
         safe += 1
     else:
-        # Supprimer chaque élément dans la line et tester chaque element en moins
-        # mettre une compteur et dire s'il y a un safe > 1 dans les ayant tous supprimer 
-        newLine = searchError(line)
-        result2 = testMethod(newLine, True)
-        if (result2 is True):
-            safe += 1 
+        newSafe = 0
+        newLines = generateNewLines(line)
+        for newLine in newLines:
+            result2 = testMethod(newLine)
+            if (result2 is True):
+                newSafe += 1 
+        if(newSafe > 0):
+            safe += 1
 
 print(f"Safe: {safe}")
-
-
-
