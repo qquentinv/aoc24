0268df8848c8ec49a62c054919b723b60caeb20b
Quentin V <50825643+qquentinv@users.noreply.github.com>
2024-12-05 16:48:22 +0000

    feat(d4): star 2 wip

---
+++ d4/star2.py
@@ -0,0 +1,99 @@
+input = [
+    "MMMSXXMASM",
+    "MSAMXMSMSA",
+    "AMXSXMAAMM",
+    "MSAMASMSMX",
+    "XMASAMXAMM",
+    "XXAMMXXAMA",
+    "SMSMSASXSS",
+    "SAXAMASAAA",
+    "MAMMMXMMMM",
+    "MXMXAXMASX"
+]
+
+#input = []
+
+#with open("input.txt", "r") as file:
+#    for line in file:
+#        input.append(line)
+
+total = 0
+
+# Trouver le mot MAS en forme de X, en diagonale haut gauche, haut droit, bas gauche, bas droite
+def findCarac(caracSearch, line):
+    pos = []
+    start = 0
+
+    while start < len(line):
+        position = line.find(caracSearch, start)
+        if position == -1:
+            break;
+        
+        pos.append(position)
+        print(f"Caractère '{caracSearch}' => position {position}.")
+        start = position + len(caracSearch)
+
+    return pos
+
+def searchInDiagonal(input, line, posFind, i): 
+    countXmas = 0
+    countDiago1 = 0
+    countDiago2 = 0
+
+    for p in posFind:
+        # Diagonale 1
+        if(i+1 < len(input) and len(line) - p >= 2 and i-1 >= 0 and len(line) - p >= 2):
+            # Regarder en haut à droite vers bas gauche
+            if(input[i+1][p+1] == "M"):
+                if(input[i-1][p-1] == "S"):
+                        print(f"--find MAS")
+                        countDiago1 += 1
+            elif(input[i+1][p+1] == "S"):
+                if(input[i-1][p-1] == "M"):
+                        print(f"--find SAM")
+                        countDiago1 += 1
+
+            if(input[i-1][p-1] == "M"):
+                if(input[i+1][p+1] == "S"):
+                        print(f"--find MAS")
+                        countDiago1 += 1
+            elif(input[i-1][p-1] == "S"):
+                if(input[i+1][p+1] == "M"):
+                        print(f"--find SAM")
+                        countDiago1 += 1
+
+
+        # Diagonale 2
+        if(i+1 < len(input) and len(line) - p >= 2 and i-1 >= 0 and len(line) - p >= 2):
+            # Regarder en haut à gauche vers bas droite
+            if(input[i-1][p+1] == "M"):
+                if(input[i+1][p-1] == "S"):
+                        print(f"--find MAS")
+                        countDiago2 += 1
+            elif(input[i-1][p+1] == "S"):
+                if(input[i+1][p-1] == "M"):
+                        print(f"--find SAM")
+                        countDiago2 += 1
+
+            if(input[i+1][p-1] == "M"):
+                if(input[i-1][p+1] == "S"):
+                        print(f"--find MAS")
+                        countDiago2 += 1
+            elif(input[i+1][p-1] == "S"):
+                if(input[i-1][p+1] == "M"):
+                        print(f"--find SAM")
+                        countDiago2 += 1
+
+        if(countDiago1 + countDiago2 > 4): 
+            countXmas += 1
+
+    return countXmas
+
+for i, line in enumerate(input):
+    print(f"ligne {i}")
+    posFind = findCarac("A", line)
+
+    if(len(posFind) > 0):
+        total += searchInDiagonal(input, line, posFind, i)
+
+print(f"total: {total}")    
