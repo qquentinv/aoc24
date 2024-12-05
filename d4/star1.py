ceb8f76d82de1e67f6082fd44f383911c18a5526
Quentin V <50825643+qquentinv@users.noreply.github.com>
2024-12-05 13:06:09 +0000

    feat(d4): star1

--- d4/star1.py
+++ d4/star1.py
@@ -11,6 +11,14 @@ input = [
     "MXMXAXMASX"
 ]
 
+input = []
+
+with open("input.txt", "r") as file:
+    for line in file:
+        input.append(line)
+
+print(input)
+
 total = 0
 
 # Trouver le mot XMAS, en diagonale haut gauche, haut droit, bas gauche, bas droite, en haut, en bas, à gauche, à droite
@@ -21,7 +29,7 @@ def findCarac(caracSearch, line):
     while start < len(line):
         position = line.find(caracSearch, start)
         if position == -1:
-            return [];
+            break;
         
         pos.append(position)
         print(f"Caractère '{caracSearch}' => position {position}.")
@@ -32,7 +40,6 @@ def findCarac(caracSearch, line):
 def searchInHorizontal(line, posFind): 
     countXmas = 0
     for p in posFind:
-        print(len(line) - p)
         if(len(line) - p >= 4):
             # Regarder à droite
             if(line[p+1] == "M"):
@@ -40,6 +47,7 @@ def searchInHorizontal(line, posFind):
                     if(line[p+3] == "S"):
                         print(f"--find droite {line[p]} {line[p+1]} {line[p+2]} {line[p+3]}")
                         countXmas += 1
+        if(p - 3 >= 0):
             # Regarder à gauche 
             if(line[p-1] == "M"):
                 if(line[p-2] == "A"):
@@ -48,10 +56,69 @@ def searchInHorizontal(line, posFind):
                         countXmas += 1
     return countXmas
 
+def searchInVertical(input, posFind, i): 
+    countXmas = 0
+    for p in posFind:
+        if(i+3 < len(input)):
+            # Regarder en bas
+            if(input[i+1][p] == "M"):
+                if(input[i+2][p] == "A"):
+                    if(input[i+3][p] == "S"):
+                        print(f"--find bas {input[i][p]} {input[i+1][p]} {input[i+2][p]} {input[i+3][p]}")
+                        countXmas += 1
+        if(i-3 >= 0):
+            # Regarder en haut
+            if(input[i-1][p] == "M"):
+                if(input[i-2][p] == "A"):
+                    if(input[i-3][p] == "S"):
+                        print(f"--find haut {input[i][p]} {input[i-1][p]} {input[i-2][p]} {input[i-3][p]}")
+                        countXmas += 1
+    return countXmas
+
+def searchInDiagonal(input, line, posFind, i): 
+    countXmas = 0
+    for p in posFind:
+        if(i+3 < len(input) and len(line) - p >= 4):
+            # Regarder en bas à droite
+            if(input[i+1][p+1] == "M"):
+                if(input[i+2][p+2] == "A"):
+                    if(input[i+3][p+3] == "S"):
+                        print(f"--find bas droite {input[i][p]} {input[i+1][p+1]} {input[i+2][p+2]} {input[i+3][p+3]}")
+                        countXmas += 1
+
+        if(i+3 < len(input) and p - 3 >= 0):
+            # Regarder en bas à gauche
+            if(input[i+1][p-1] == "M"):
+                if(input[i+2][p-2] == "A"):
+                    if(input[i+3][p-3] == "S"):
+                        print(f"--find bas gauche {input[i][p]} {input[i+1][p-1]} {input[i+2][p-2]} {input[i+3][p-3]}")
+                        countXmas += 1
+
+        if(i-3 >= 0 and len(line) - p >= 4):
+            # Regarder en haut à droite
+            if(input[i-1][p+1] == "M"):
+                if(input[i-2][p+2] == "A"):
+                    if(input[i-3][p+3] == "S"):
+                        print(f"--find haut droite {input[i][p]} {input[i-1][p+1]} {input[i-2][p+2]} {input[i-3][p+3]}")
+                        countXmas += 1
+
+        if(i-3 >= 0 and p - 3 >= 0):
+            # Regarder en haut à gauche
+            if(input[i-1][p-1] == "M"):
+                if(input[i-2][p-2] == "A"):
+                    if(input[i-3][p-3] == "S"):
+                        print(f"--find haut gauche {input[i][p]} {input[i-1][p-1]} {input[i-2][p-2]} {input[i-3][p-3]}")
+                        countXmas += 1
+        
+    return countXmas
+
 for i, line in enumerate(input):
     print(f"ligne {i}")
     posFind = findCarac("X", line)
+
     if(len(posFind) > 0):
         total += searchInHorizontal(line, posFind)
+        total += searchInVertical(input, posFind, i)
+        total += searchInDiagonal(input, line, posFind, i)
 
 print(f"total: {total}")    
